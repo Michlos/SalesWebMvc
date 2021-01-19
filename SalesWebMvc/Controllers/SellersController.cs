2f21bde00525ded7c9bf1c5f5139ace363ac8ab8
MichlosBSB <michlos@gmail.com>
2021-01-19 14:49:11 -0300

    Formulário de Criação Simples

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 
+using SalesWebMvc.Models;
 using SalesWebMvc.Services;
 
 using System;
@@ -20,6 +21,17 @@ namespace SalesWebMvc.Controllers
         {
             var list = _sellerService.FindAll();
             return View(list);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Seller seller)
+        {
+            _sellerService.Insert(seller);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
